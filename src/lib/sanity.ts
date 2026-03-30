a6185e1ecb12608f8ff149d627b8e748cc05fb93
srilu396 <srilunagulapalli396@gmail.com>
2026-03-30 15:04:54 +0530

    fix vercel peer dependency conflict

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'next-sanity'
-import imageUrlBuilder from '@sanity/image-url'
+import { createImageUrlBuilder } from "@sanity/image-url";
 import type { SanityImageSource } from '@sanity/image-url'
 
 export const client = createClient({
@@ -9,7 +9,7 @@ export const client = createClient({
   useCdn: false,
 })
 
-const builder = imageUrlBuilder(client)
+const builder = createImageUrlBuilder(client);
 
 export const urlFor = (source: SanityImageSource) =>
   builder.image(source)
